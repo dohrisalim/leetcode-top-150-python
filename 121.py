70035b6e447f29608af37e4dd66f7620e7d4097e
Salim Dohri <dohri.salim@gmail.com>
2024-01-01 22:00:29 +0100

    Add solution for maximum profit in stock prices

---
+++ 121.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices):
+        min_price = float('inf')
+        max_profit = 0
+
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = price - min_price
+            max_profit = max(max_profit, profit)
+
+        return max_profit
