4c915675fbd3dd785cd1bb839620def9068da977
Salim Dohri <dohri.salim@gmail.com>
2024-01-03 20:31:29 +0100

    Add intToRoman method to Solution class

---
+++ 12.py
@@ -0,0 +1,11 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        
+        roman = ""
+        for value, symbol in zip(values, symbols):
+            while num >= value:
+                num -= value
+                roman += symbol
+        return roman
