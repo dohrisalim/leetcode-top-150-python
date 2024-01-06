f7e4f80cd77b14cbe1f29e0e7db4543eb247aec5
Salim Dohri <dohri.salim@gmail.com>
2024-01-06 10:09:31 +0100

    Add matrix rotation solution

---
+++ 48.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+        for i in range(n):
+            matrix[i] = matrix[i][::-1]
