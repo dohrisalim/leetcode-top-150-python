b0e01bae5c4e122f0153da70f90457ef974f9bfc
Salim Dohri <dohri.salim@gmail.com>
2024-01-06 17:05:06 +0100

    Add solution for grouping anagrams

---
+++ 49.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram = {}
+
+        for word in strs:
+
+            temp_word = ''.join(sorted(word))
+
+            if temp_word in anagram:
+                anagram[temp_word].append(word)
+
+            else:
+                anagram[temp_word] = [word]  
+
+        return list(anagram.values())
