a1e27af5a1c05230ec179589367db73d245ebd10
Salim Dohri <dohri.salim@gmail.com>
2024-01-06 11:15:21 +0100

    Add Game of Life implementation

---
+++ 289.py
@@ -0,0 +1,22 @@
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        m, n = len(board), len(board[0])
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]
+        
+        for i in range(m):
+            for j in range(n):
+                live_neighbors = 0
+                for d in directions:
+                    ni, nj = i + d[0], j + d[1]
+                    if 0 <= ni < m and 0 <= nj < n and abs(board[ni][nj]) == 1:
+                        live_neighbors += 1
+                if board[i][j] == 1 and (live_neighbors < 2 or live_neighbors > 3):
+                    board[i][j] = -1
+                if board[i][j] == 0 and live_neighbors == 3:
+                    board[i][j] = 2
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] > 0:
+                    board[i][j] = 1
+                else:
+                    board[i][j] = 0
